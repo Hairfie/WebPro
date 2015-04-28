db600bdb5f6d4a85c944f24381f5b5b0585e3e59
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-04-28 19:30:20 +0200

    map in progress

--- src/pages/BusinessMapPage.js
+++ src/pages/BusinessMapPage.js
@@ -7,6 +7,9 @@ import { connectToStores } from 'fluxible/addons';
 import Link, {FlatLink} from '../components/Link';
 import BusinessActions from '../actions/BusinessActions';
 import _ from 'lodash';
+import GetGoogleMaps from '../plugins/getGoogleMap';
+
+const DEFAULT_LOCATION = {lat: 48.867439, lng: 2.343644};
 
 class AddressInputGroup extends React.Component {
     render () {
@@ -35,6 +38,18 @@ class AddressInputGroup extends React.Component {
 }
 
 class BusinessMapPage extends React.Component {
+    constructor(props) {
+        super(props);
+        const { business: {gps} } = this.props;
+
+        this.state = {
+            location        : gps || DEFAULT_LOCATION,
+            map             : null,
+            marker          : null,
+            markerHasMoved  : false
+        };
+    }
+
     static contextTypes = {
         executeAction: React.PropTypes.func.isRequired
     }
@@ -42,16 +57,52 @@ class BusinessMapPage extends React.Component {
     render() {
         const { business } = this.props;
 
+        this.updateMap();
+
         return (
             <Layout ref="layout" {...this.props}>
                 <h1>Adresse & Carte</h1>
                 <AddressInputGroup address={business.address} ref="address" />
                 <br />
+                <div ref="map" style={{width: '100%', minHeight: '200px'}}/>
                 <FlatButton label='Sauver les modifications' onClick={this.save} />
                 {' ou '}
                 <FlatLink route="business" params={{ businessId: business.id }} label='Annuler' />
             </Layout>
         );
+    }
+
+    componentDidMount() {
+        createMap(this.refs.map.getDOMNode())
+            .then(function (map) {
+                this.setState({map: map});
+                return createMarker(map, this.onMarkerMoved);
+            }.bind(this))
+            .then(function (marker) {
+                this.setState({marker: marker});
+            }.bind(this));
+    }
+
+    updateMap() {
+        if (this.state.map && !this.state.markerHasMoved) {
+            this.state.map.panTo(this.state.location);
+        }
+        if (this.state.marker) {
+            this.state.marker.setPosition(this.state.location);
+        }
+    }
+
+    onMarkerMoved = (marker) => {
+
+        this.setState({
+            markerHasMoved: true,
+            location      : {
+                lat: this.state.marker.position.lat(),
+                lng: this.state.marker.position.lng()
+            }
+        });
+        debugger;
+        this.props.onChange();
     }
 
     save = () => {
@@ -64,6 +115,29 @@ class BusinessMapPage extends React.Component {
 
         this.context.executeAction(BusinessActions.updateInfos, { businessId, values });
     }
+}
+
+function createMap(el) {
+    var options = {};
+    options.zoom = 16;
+    return GetGoogleMaps()
+        .then(function (google) {
+            return new google.maps.Map(el, options);
+        });
+}
+
+function createMarker(map, onMoved) {
+    var options = {};
+    options.map = map;
+    options.position = map.getCenter();
+    options.draggable = true;
+
+    return GetGoogleMaps()
+        .then(function (google) {
+            var marker = new google.maps.Marker(options);
+            google.maps.event.addListener(marker, 'dragend', onMoved);
+            return marker;
+        });
 }
 
 BusinessMapPage = connectToStores(BusinessMapPage, [
