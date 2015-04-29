7c47ff9cdfdd25d7fdd2bc4aab34ba6eecb85bd6
Antoine Hérault <antoine.herault@gmail.com>
2015-04-29 11:48:05 +0200

    Add facebook button

---
+++ src/components/FacebookLoginButton.js
@@ -0,0 +1,39 @@
+'use strict';
+
+import React from 'react';
+import Facebook from '../services/facebook';
+import AuthActions from '../actions/AuthActions';
+import { RaisedButton } from 'material-ui';
+
+export default class FacebookLoginButton extends React.Component {
+    static contextTypes = {
+        executeAction: React.PropTypes.func.isRequired,
+    }
+    constructor(props) {
+        super(props);
+        this.state = { sdk: null };
+    }
+    componentDidMount() {
+        Facebook.getSdk().then(sdk => this.setState({ sdk }));
+    }
+    render() {
+        const {
+            onTouchTap,
+            onClick,
+            ...otherProps
+        } = this.props;
+
+        const waitingForSdk = !this.state.sdk;
+
+        return <RaisedButton
+            label="Se connecter via Facebook"
+            onTouchTap={this.onTouchTap}
+            disabled={waitingForSdk}
+            {...otherProps}
+            />;
+    }
+    onTouchTap = (e) => {
+        e.preventDefault();
+        this.state.sdk.login(response => this.context.executeAction(AuthActions.loginWithFacebook, { response }));
+    }
+}
