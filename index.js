f632b120b753f2157fd0023bd693086b4742ce6a
Antoine Hérault <antoine.herault@gmail.com>
2015-04-29 20:51:09 +0200

    Add newrelic

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+if (process.env.NEW_RELIC_LICENSE_KEY) {
+    require('newrelic');
+}
 
 // Register babel to have ES6 support on the server
-require("babel/register")({stage: 0});
+require('babel/register')({stage: 0});
 
 // Start the server app
-require("./src/server");
+require('./src/server');
